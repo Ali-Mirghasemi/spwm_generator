f38eb80a5606fd9e73f5d84a1b696134d4d8a676
Ali <ali.mirghasemi1376@gmail.com>
2022-11-30 01:11:06 +0330

    Add format for raw hex

---
+++ src/format/raw_hex.rs
@@ -0,0 +1,26 @@
+use std::io::{BufWriter, Result, Write};
+
+use spwm_generator::SPWM;
+
+use super::Format;
+
+
+
+#[derive(Default)]
+pub struct RawHex;
+
+impl Format for RawHex {
+    fn write(&self, spwm: &SPWM, width: usize, sep: &str, buf: &mut BufWriter<std::fs::File>) -> Result<()> {
+        let table = spwm.lookup_table();
+
+        for row in table.chunks(width) {
+            for val in row {
+                write!(buf, "{:04X}{}", val, sep)?;
+            }
+            writeln!(buf, "")?;
+        }
+        
+        Ok(())
+    }
+}
+
