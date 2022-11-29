ae9d760572084d5696984cb2acd8d3c88a48e539
Ali <ali.mirghasemi1376@gmail.com>
2022-11-30 01:11:23 +0330

    Add format for rust file

---
+++ src/format/rust.rs
@@ -0,0 +1,83 @@
+use std::io::{BufWriter, Result, Write};
+
+use spwm_generator::SPWM;
+
+use super::Format;
+
+#[derive(Default)]
+pub struct RustFile;
+
+impl Format for RustFile {
+    fn write(&self, spwm: &SPWM, width: usize, _sep: &str, buf: &mut BufWriter<std::fs::File>) -> Result<()> {
+        let table = spwm.lookup_table();
+
+        let (ty, pad_width) = if spwm.pwm_top() >= 65536 {
+            ("u32", 6)
+        }
+        else if spwm.pwm_top() >= 256 {
+            ("u16", 5)
+        }
+        else {
+            ("u8", 3)
+        };
+
+        writeln!(buf, "const TABLE_{}_{}[{}; {}] = [", 
+            spwm.sin_freq(), 
+            spwm.carrier_freq(),
+            ty,
+            table.len()
+        )?;
+
+        for row in table.chunks(width) {
+            write!(buf, "    ")?;
+            for val in row {
+                write!(buf, "{:width$}, ", val, width=pad_width)?;
+            }
+            writeln!(buf, "")?;
+        }
+        
+        writeln!(buf, "];")?;
+
+        Ok(())
+    }
+}
+
+#[derive(Default)]
+pub struct RustHexFile;
+
+impl Format for RustHexFile {
+    fn write(&self, spwm: &SPWM, width: usize, _sep: &str, buf: &mut BufWriter<std::fs::File>) -> Result<()> {
+        let table = spwm.lookup_table();
+
+        let (ty, pad_width) = if spwm.pwm_top() >= 65536 {
+            ("u32", 8)
+        }
+        else if spwm.pwm_top() >= 256 {
+            ("u16", 4)
+        }
+        else {
+            ("u8", 2)
+        };
+
+        writeln!(buf, "const TABLE_{}_{}[{}; {}] = [", 
+            spwm.sin_freq(), 
+            spwm.carrier_freq(),
+            ty,
+            table.len()
+        )?;
+
+        for row in table.chunks(width) {
+            write!(buf, "    ")?;
+            for val in row {
+                write!(buf, "0x{:0width$X}, ", val, width=pad_width)?;
+            }
+            writeln!(buf, "")?;
+        }
+        
+        writeln!(buf, "];")?;
+
+        Ok(())
+    }
+}
+
+
