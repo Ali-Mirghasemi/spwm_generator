6362ff064de0eb15056421f804122e21fd667a56
Ali <ali.mirghasemi1376@gmail.com>
2022-11-30 01:12:20 +0330

    Implement main algorithm

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::{io::{BufWriter}, fs::File};
+
+use args::Args;
+use clap::Parser;
+use spwm_generator::*;
+
+mod args;
+mod format;
+
+use format::*;
+
+fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+
+    let spwm = SPWM::new(
+        args.sin_freq, 
+        args.carrier_freq, 
+        args.pwm_top, 
+        args.padding
+    );
+    
+    let fs = File::create(&args.output)?;
+    let mut buf = BufWriter::new(fs);
+    let writer: &dyn Format = match args.format {
+        args::Format::Raw => &Raw,
+        args::Format::RawHex => &RawHex,
+        args::Format::C => &CFile,
+        args::Format::CHex => &CHexFile,
+        args::Format::Rust => &RustFile,
+        args::Format::RustHex => &RustHexFile,
+    };
+    writer.write(&spwm, args.row_width, &args.separator, &mut buf)
+}
