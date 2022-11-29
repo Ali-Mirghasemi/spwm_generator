a1b1919fbc027f46b533e8f60c9d2ca45c7ad54a
Ali <ali.mirghasemi1376@gmail.com>
2022-11-30 01:10:21 +0330

    Add args module

---
+++ src/args.rs
@@ -0,0 +1,36 @@
+use std::path::PathBuf;
+
+use clap::{Parser, ValueEnum};
+
+
+
+#[derive(Parser)]
+#[clap(author, version, about)]
+pub struct Args {
+    #[clap(short = 'f', long = "sin_freq", help = "sin wave frequency")]
+    pub sin_freq:           f64,
+    #[clap(short = 'c', long = "carrier_freq", help = "carrier frequency")]
+    pub carrier_freq:       f64,
+    #[clap(short = 't', long = "pwm_top", default_value = "255", help = "pwm top value")]
+    pub pwm_top:            usize,
+    #[clap(short = 'p', long = "padding", default_value = "0", help = "padding for pwm min and max value")]
+    pub padding:            usize,
+    #[clap(short = 'o', long = "out", help = "output file path")]
+    pub output:             PathBuf,
+    #[clap(short = 'm', long = "format", default_value = "raw", help = "format")]
+    pub format:             Format,
+    #[clap(short = 'w', long = "row_width", default_value = "16", help = "number of samples in row")]
+    pub row_width:          usize,
+    #[clap(short = 's', long = "separator", default_value = ", ", help = "separator character")]
+    pub separator:          String, 
+}
+
+#[derive(Clone, Copy, ValueEnum)]
+pub enum Format {
+    Raw,
+    RawHex,
+    C,
+    CHex,
+    Rust,
+    RustHex,
+}
