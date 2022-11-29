a092e82e8ced3d0e17df1fd47e390a3dd01d6a7b
Ali <ali.mirghasemi1376@gmail.com>
2022-11-30 01:10:52 +0330

    Add format for raw

---
+++ src/format/raw.rs
@@ -0,0 +1,27 @@
+use std::io::{BufWriter, Result, Write};
+
+use spwm_generator::SPWM;
+
+use super::Format;
+
+
+
+
+#[derive(Default)]
+pub struct Raw;
+
+impl Format for Raw {
+    fn write(&self, spwm: &SPWM, width: usize, sep: &str, buf: &mut BufWriter<std::fs::File>) -> Result<()> {
+        let table = spwm.lookup_table();
+
+        for row in table.chunks(width) {
+            for val in row {
+                write!(buf, "{:3}{}", val, sep)?;
+            }
+            writeln!(buf, "")?;
+        }
+        
+        Ok(())
+    }
+}
+
