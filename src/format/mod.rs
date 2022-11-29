1e304e9a36162b84dab43f7c0ef77468d2e38e07
Ali <ali.mirghasemi1376@gmail.com>
2022-11-30 01:11:49 +0330

    Finilize format module

---
+++ src/format/mod.rs
@@ -0,0 +1,20 @@
+use std::{io::BufWriter, fs::File};
+
+use spwm_generator::SPWM;
+
+
+
+mod raw;
+mod raw_hex;
+mod c;
+mod rust;
+
+pub use raw::*;
+pub use raw_hex::*;
+pub use c::*;
+pub use rust::*;
+
+pub trait Format {
+    fn write(&self, spwm: &SPWM, width: usize, sep: &str, buf: &mut BufWriter<File>) -> std::io::Result<()>;
+}
+
