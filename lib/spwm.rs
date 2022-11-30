3fc58c960d1d7a7584365db46f763783e1a7a7ae
Ali <ali.mirghasemi1376@gmail.com>
2022-11-30 20:48:24 +0330

    Replace carrier_freq with num_of_samples

--- lib/spwm.rs
+++ lib/spwm.rs
@@ -4,31 +4,31 @@ use crate::{PWM, SinWave};
 /// Hold SPWM parameters
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct SPWM {
-    pwm:            PWM,
-    carrier_freq:   f64,
+    pwm:                PWM,
+    num_of_samples:     usize,
 }
 
 
 impl SPWM {
     /// create new instance
-    pub fn new(sin_freq: f64, carrier_freq: f64, pwm_top: usize, padding: usize) -> Self{
+    pub fn new(sin_freq: f64, num_of_samples: usize, pwm_top: usize, padding: usize) -> Self{
         Self { 
             pwm: PWM::new(
                 SinWave::new(sin_freq, 1.0), 
                 pwm_top, padding
             ), 
-            carrier_freq,
+            num_of_samples: num_of_samples,
         }
     }
 
     /// return carrier frequency
-    pub fn carrier_freq(&self) -> f64 {
-        self.carrier_freq
+    pub fn num_of_samples(&self) -> usize {
+        self.num_of_samples
     }
 
     /// set carrier frequency
-    pub fn set_carrier_freq(&mut self, carrier_freq: f64) {
-        self.carrier_freq = carrier_freq;
+    pub fn set_num_of_samples(&mut self, num_of_samples: usize) {
+        self.num_of_samples = num_of_samples;
     }
 
     pub fn sin_freq(&self) -> f64 {
@@ -41,7 +41,7 @@ impl SPWM {
 
     /// generate lookup table
     pub fn lookup_table(&self) -> Vec<usize> {
-        self.pwm.duty_cycles_range(0.0, 1.0, 1.0 / self.carrier_freq)
+        self.pwm.duty_cycles_fixed(0.0, 1.0, self.num_of_samples)
     }
 }
 
