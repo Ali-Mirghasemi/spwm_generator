59d64a29640ed2574344994298593a920fd88110
Ali <ali.mirghasemi1376@gmail.com>
2022-12-19 20:08:37 +0330

    Add user sections

--- src/format/rust.rs
+++ src/format/rust.rs
@@ -1,14 +1,14 @@
-use std::io::{BufWriter, Result, Write};
+use std::{io::{BufWriter, Result, Write, Seek}, fs::File};
 
 use spwm_generator::SPWM;
 
-use super::Format;
+use super::{Format, UserSection, FormatArgs};
 
 #[derive(Default)]
 pub struct RustFile;
 
 impl Format for RustFile {
-    fn write(&self, name: &str, spwm: &SPWM, width: usize, _sep: &str, buf: &mut BufWriter<std::fs::File>) -> Result<()> {
+    fn write(&self, spwm: &SPWM, buf: &mut File, args: &FormatArgs) -> Result<()> {
         let table = spwm.lookup_table();
 
         let (ty, pad_width) = if spwm.pwm_top() >= 65536 {
@@ -21,14 +21,20 @@ impl Format for RustFile {
             ("u8", 3)
         };
 
-        writeln!(buf, "const {}_{}[{}; {}] = [", 
-            name,
+        let sections = UserSection::read_user_reign(buf)?;
+        buf.rewind()?;
+        buf.set_len(0)?;
+
+        
+        sections.write(0, buf)?;
+        writeln!(buf, "const {}_{}HZ[{}; {}] = [", 
+            args.name,
             spwm.sin_freq(), 
             ty,
             table.len()
         )?;
 
-        for row in table.chunks(width) {
+        for row in table.chunks(args.width) {
             write!(buf, "    ")?;
             for val in row {
                 write!(buf, "{:width$}, ", val, width=pad_width)?;
@@ -37,6 +43,33 @@ impl Format for RustFile {
         }
         
         writeln!(buf, "];")?;
+        sections.write(1, buf)?;
+
+        if let Some(pad) = args.inverter {
+            let table = spwm.table_not(&table, pad);
+
+            writeln!(buf, "const {}_{}HZ_NOT[{}; {}] = [", 
+                args.name,
+                spwm.sin_freq(), 
+                ty,
+                table.len()
+            )?;
+
+            for row in table.chunks(args.width) {
+                write!(buf, "    ")?;
+                for val in row {
+                    write!(buf, "{:width$}, ", val, width=pad_width)?;
+                }
+                writeln!(buf, "")?;
+            }
+            
+            writeln!(buf, "];")?;
+        }
+
+
+
+        sections.write(2, buf)?;
+        sections.write_remains(3, buf)?;
 
         Ok(())
     }
@@ -46,7 +79,7 @@ impl Format for RustFile {
 pub struct RustHexFile;
 
 impl Format for RustHexFile {
-    fn write(&self, name: &str, spwm: &SPWM, width: usize, _sep: &str, buf: &mut BufWriter<std::fs::File>) -> Result<()> {
+    fn write(&self, spwm: &SPWM, buf: &mut File, args: &FormatArgs) -> Result<()> {
         let table = spwm.lookup_table();
 
         let (ty, pad_width) = if spwm.pwm_top() >= 65536 {
@@ -59,14 +92,19 @@ impl Format for RustHexFile {
             ("u8", 2)
         };
 
-        writeln!(buf, "const {}_{}[{}; {}] = [", 
-            name,
+        let sections = UserSection::read_user_reign(buf)?;
+        buf.rewind()?;
+        buf.set_len(0)?;
+
+        sections.write(0, buf)?;
+        writeln!(buf, "const {}_{}HZ[{}; {}] = [", 
+            args.name,
             spwm.sin_freq(), 
             ty,
             table.len()
         )?;
 
-        for row in table.chunks(width) {
+        for row in table.chunks(args.width) {
             write!(buf, "    ")?;
             for val in row {
                 write!(buf, "0x{:0width$X}, ", val, width=pad_width)?;
@@ -75,6 +113,31 @@ impl Format for RustHexFile {
         }
         
         writeln!(buf, "];")?;
+        sections.write(1, buf)?;
+
+        if let Some(pad) = args.inverter {
+            let table = spwm.table_not(&table, pad);
+
+            writeln!(buf, "const {}_{}HZ_NOT[{}; {}] = [", 
+                args.name,
+                spwm.sin_freq(), 
+                ty,
+                table.len()
+            )?;
+
+            for row in table.chunks(args.width) {
+                write!(buf, "    ")?;
+                for val in row {
+                    write!(buf, "0x{:0width$X}, ", val, width=pad_width)?;
+                }
+                writeln!(buf, "")?;
+            }
+            
+            writeln!(buf, "];")?;
+        }
+
+        sections.write(2, buf)?;
+        sections.write_remains(3, buf)?;
 
         Ok(())
     }
