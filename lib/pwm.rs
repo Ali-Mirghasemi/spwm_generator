6159ae87c542fc2dd2f4b67bbf7d9ff1dac14c90
Ali <ali.mirghasemi1376@gmail.com>
2022-12-19 20:07:35 +0330

    Update types

--- lib/pwm.rs
+++ lib/pwm.rs
@@ -6,13 +6,15 @@ use rayon::prelude::*;
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct PWM {
     wave:           SinWave,
-    pwm_top:        usize,
-    padding:        usize,
+    pwm_top:        DutyCycle,
+    padding:        DutyCycle,
 }
+
+pub type DutyCycle = i32;
 
 impl PWM {
     /// create new instance
-    pub fn new(mut wave: SinWave, pwm_top: usize, padding: usize) -> Self {
+    pub fn new(mut wave: SinWave, pwm_top: DutyCycle, padding: DutyCycle) -> Self {
         wave.set_offset(0.0);
         Self {
             wave,
@@ -26,7 +28,7 @@ impl PWM {
         self.wave.freq()
     }
 
-    pub fn pwm_top(&self) -> usize {
+    pub fn pwm_top(&self) -> DutyCycle {
         self.pwm_top
     }
 
@@ -45,12 +47,12 @@ impl PWM {
     /// assert!(val, 255);
     /// ```
     /// 
-    pub fn duty_cycle(&self, time: f64) -> usize {
-        let mut val = ((self.pwm_top as f64 / 2.0) + ((self.wave.sample(time) * (self.pwm_top as f64 / 2.0)) / self.wave.amplitude())) as usize;
+    pub fn duty_cycle(&self, time: f64) -> DutyCycle {
+        let mut val = ((self.pwm_top as f64 / 2.0) + ((self.wave.sample(time) * (self.pwm_top as f64 / 2.0)) / self.wave.amplitude())) as DutyCycle;
         if val + self.padding > self.pwm_top {
             val = self.pwm_top - self.padding;
         }
-        else if val - self.padding > self.pwm_top {
+        else if val - self.padding < 0 {
             val = self.padding;
         }
         val
@@ -65,7 +67,7 @@ impl PWM {
     /// let duty_cycles = pwm.duty_cycles_range(0.0, 1.0, 0.125);
     /// println!("{:?}", duty_cycles);
     /// ```
-    pub fn duty_cycles_range(&self, start_time: f64, end_time: f64, step: f64) -> Vec<usize> {
+    pub fn duty_cycles_range(&self, start_time: f64, end_time: f64, step: f64) -> Vec<DutyCycle> {
         let num_samples = ((end_time - start_time) / step) as usize;
 
         self.duty_cycles(start_time, num_samples, step)
@@ -80,7 +82,7 @@ impl PWM {
     /// let duty_cycles = pwm.duty_cycles_fixed(0.0, 1.0, 8);
     /// println!("{:?}", duty_cycles);
     /// ```
-    pub fn duty_cycles_fixed(&self, start_time: f64, end_time: f64, num_samples: usize) -> Vec<usize> {
+    pub fn duty_cycles_fixed(&self, start_time: f64, end_time: f64, num_samples: usize) -> Vec<DutyCycle> {
         let step = (end_time - start_time) / num_samples as f64;
         
         self.duty_cycles(start_time, num_samples, step)
@@ -95,7 +97,7 @@ impl PWM {
     /// let duty_cycles = pwm.duty_cycles(0.0, 8, 0.125);
     /// println!("{:?}", duty_cycles);
     /// ```
-    pub fn duty_cycles(&self, start_time: f64, num_samples: usize, step: f64) -> Vec<usize> {
+    pub fn duty_cycles(&self, start_time: f64, num_samples: usize, step: f64) -> Vec<DutyCycle> {
         (0..num_samples).into_par_iter()
             .map(|x| {
                 self.duty_cycle(start_time + x as f64 * step)
