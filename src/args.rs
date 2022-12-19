a34356cddc62ce52751a617e0c3341c00a26dd20
Ali <ali.mirghasemi1376@gmail.com>
2022-12-19 20:08:19 +0330

    Add more options

--- src/args.rs
+++ src/args.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use clap::{Parser, ValueEnum};
+use spwm_generator::DutyCycle;
 
 
 
@@ -10,9 +11,13 @@ pub struct Args {
     #[clap(short = 'f', long = "sin_freq", help = "sin wave frequency")]
     pub sin_freq:           f64,
     #[clap(short = 'n', long = "num_of_samples", help = "number of samples")]
-    pub num_of_samples:     usize,
+    pub num_of_samples:     Option<usize>,
+    #[clap(short = 'c', long = "carrier_freq", help = "carrier frequency or PWM frequency")]
+    pub carrier_freq:       Option<f64>,
+    #[clap(short = 's', long = "step", help = "time step in seconds")]
+    pub step:               Option<f64>,
     #[clap(short = 't', long = "pwm_top", default_value = "255", help = "pwm top value")]
-    pub pwm_top:            usize,
+    pub pwm_top:            DutyCycle,
     #[clap(short = 'p', long = "padding", default_value = "0", help = "padding for pwm min and max value")]
     pub padding:            usize,
     #[clap(short = 'o', long = "out", help = "output file path")]
@@ -21,10 +26,18 @@ pub struct Args {
     pub format:             Format,
     #[clap(short = 'w', long = "row_width", default_value = "16", help = "number of samples in row")]
     pub row_width:          usize,
-    #[clap(short = 's', long = "separator", default_value = ", ", help = "separator character")]
+    #[clap(short = 'S', long = "separator", default_value = ", ", help = "separator character")]
     pub separator:          String, 
     #[clap(short = 'a', long = "name", default_value = "WAVE", help = "name of variable")]
     pub name:               String, 
+    #[clap(short = 'C', long = "cycle", help = "lookup table just for one cycle, this parameter override duration parameter")]
+    pub one_cycle:          bool,
+    #[clap(short = 'd', long = "duration", default_value = "1.0", help = "duration of lookup table")]
+    pub duration:           f64,
+    #[clap(short = 'P', long = "plot", help = "plot spwm wave")]
+    pub plot:               Option<PlotMode>,
+    #[clap(short = 'I', long = "inverter", help = "inverter mode")]
+    pub inverter:           Option<f64>,
 }
 
 #[derive(Clone, Copy, ValueEnum)]
@@ -35,4 +48,10 @@ pub enum Format {
     CHex,
     Rust,
     RustHex,
+}
+
+#[derive(Clone, Copy, ValueEnum)]
+pub enum PlotMode {
+    PWM,
+    CenterAligned,
 }
